100a511a618af18a9c1bdc24c603803fa2fdb606
Raziq Kassam <raziq.kassam@gmail.com>
2021-02-23 18:04:40 -0700

    Updated visual art section with hover images, changed website colors

---
+++ src/components/Headers/HeaderData.js
@@ -0,0 +1,14 @@
+export const Header_Experience = {
+    id: "experience",
+    header: "EXPERIENCE"
+}
+
+export const Header_Projects = {
+    id: "projects",
+    header: "PROJECTS"
+}
+
+export const Header_Skills = {
+    id: "skills",
+    header: "SKILLS"
+}
